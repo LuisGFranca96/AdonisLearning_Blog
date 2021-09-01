c7ca57cad0b8f58378def1dd9005ef8b9cd05682
Luis Guilherme <luisdkd@hotmail.com>
2021-09-01 19:36:37 -0300

    primeiro commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,61 @@
+//  import { Response } from '@adonisjs/core/build/standalone';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema,rules} from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User';
+//import authConfig from 'Config/auth';
+
+export default class SignupController {
+    public async index({ request, response }:HttpContextContract){
+        const req = await request.validate({ 
+            schema:schema.create({
+            name: schema.string(),
+            email: schema.string({},[
+                rules.email(),
+            ]),
+            password: schema.string({},[
+                rules.confirmed()
+            ]),
+            
+        }),
+        messages: {
+            'name.required':'Por favor digite o nome',
+            'email.required':'Por favor digite o email',
+            'password.required':'Por favor digite uma senha',
+        }
+    })
+        const user = new User()
+        user.name = req.name
+        user.email = req.email
+        user.password = req.password
+        await user.save();
+        return response.redirect('/')
+    }
+
+    public async login({ request, auth, response }:HttpContextContract){
+        const req = await request.validate({
+            schema:schema.create({
+                email: schema.string({},[
+                    rules.email(),
+                ]),
+                password: schema.string({},[
+                    rules.minLength(3)
+                ])
+            }),
+            messages: {
+                'email.required':'Por favor digite o email',
+                'password.required':'Por favor digite a senha',
+                'password.minLength':'Senha precisa ter pelo menos 3 caracteres'
+            }
+        })
+        const email = req.email
+        const password= req.password
+        await auth.attempt(email,password)          
+        return response.redirect('/profile')
+
+    }
+
+    public async logout({ auth, response }:HttpContextContract){
+        await auth.logout()
+        return response.redirect('/')
+    }
+}
